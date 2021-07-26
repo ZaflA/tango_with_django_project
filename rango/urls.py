8682c394f2e8c43448f427cee0abcf9224f2fe28
“ZaflA” <13051659722@163.com>
2021-07-27 00:28:13 +0800

    chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rango import views
+app_name = 'rango'
+urlpatterns = [
+path('', views.index, name='index'),
+path('about/', views.about),
+#对，这样写就直接是下一级的第一个目录了
+]
