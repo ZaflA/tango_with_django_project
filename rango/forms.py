b7157fc270d1e8e59350b3a3b2fb8bad4b74450d
ZaflA <13051659722@163.com>
2021-07-30 13:43:52 +0800

    chapter 9 1st commit

--- rango/forms.py
+++ rango/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
+
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, 
     help_text="Please enter the category name.") 
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0) 
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0) 
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
     # An inline class to provide additional information on the form.
     class Meta:
         # Provide an association between the ModelForm and a model
@@ -16,7 +19,6 @@ class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
 
     class Meta:
         # Provide an association between the ModelForm and a model
@@ -39,3 +41,15 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/',views.add_page, name='add_page'),
-    #对，这样写就直接是下一级的第一个目录了
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('restricted/', views.restricted, name='restricted'),
+    path('logout/', views.user_logout, name='logout'),
+    #directly write the dir name
 ]
